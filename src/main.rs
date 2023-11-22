4807523a3af8a44bd098d19717cc164fa6ed48e5
Phantola <phantoladev@gmail.com>
2023-11-22 15:58:41 +0900

    Add code for ownership

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
     // let x = "string";
     // println!("{}", &x);
 
-    // variable types
+    // Variable types
     // Scalar types
     const BITS_PER_BYTE: usize = 8;
     println!(
@@ -58,7 +58,6 @@ fn main() {
     println!("{}", c);
 
     // Tuple.
-
     let t: (i32, f64, u8) = (500, 1.4, 4);
     println!("{} {} {}", t.0, t.1, t.2);
 
@@ -69,11 +68,10 @@ fn main() {
     // Array and vector.
     // Array is Fixed length, using stack with bunch of memory
     // vetor is dynamic langth cuz' using heap area
-
     let a = [1, 2, 3, 4];
     println!("{}", a[0]); // error occured when runtime. *panic*
 
-    // function
+    // Function
     let str: &str = "String is here";
     println!("str length : {} characters", another_function(str));
 
@@ -84,20 +82,56 @@ fn main() {
         number = number - 1;
     }
 
-    println!("for statement test");
+    println!("'For' statement test");
     for n in (1..11).rev() {
         // .rev() is reverse Range
         println!("{}", n);
     }
 
-    println!("for statement with collection iterator test");
+    println!("'For' statement with collection iterator test");
     let arr = [1, 2, 3, 4, 5];
     for n in arr.iter() {
         println!("{}", n);
     }
 
+    // Type of function implement
+    println!("Typeof test");
     let x = 3;
     println!("{}", type_of(&x));
+
+    // Ownership
+    // String type can append, but string literal is not.
+    // Cuz, String literal value can specified in compile time, so hard-coded in final executable file
+    println!("Ownership test :: String");
+    let mut s = String::from("Hello rust wold!");
+    s.push_str(", made by phantola");
+    println!("{}", s);
+
+    {
+        #[warn(unused_variables)]
+        let a = String::from("hello this is local string");
+        // something do with a
+    }
+    // vairable 'a' is not more usable. Rust free memory when meet '}' automatically
+
+    // 러스트는 아래와 같이 기존의 heap 을 사용하는 변수를 다른 변수에 할당하게 되면,
+    // 복사가 아니라 move (이동) 이 일어난다. 따라서 s 를 출력하려고 하면 아예 동작하지 않고
+    // 에러뱉음.
+    // let s2 = s; 하는 순간, s의 포인터가 무효화되고, s2 의 포인터가 바인딩 되기 때문이다.
+    let s2 = s;
+    // Error occurred!
+    // print!("{}", s);
+
+    // 만약 정말 deep copy 하고 싶다면,
+    // 이렇게 .clone() method 사용해야함
+    let s3 = s2.clone();
+    println!("{}", s3);
+
+    // Normal stackable variable (int, float...) has 'Copy' Trait.
+    // If some type has Copy trait, it can be use after re-assign like downward
+    let a = 3;
+    let b = a;
+    println!("{}, {}", a, b); // OK
 }
 
 // funciton name should be snake case with small case for convention
